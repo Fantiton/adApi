a8c65a26ad0d442a1782169026f0ea6069f1c5cb
Filip <filipcelejewski@wp.pl>
2024-10-18 09:11:49 +0200

    user class login

--- adApi/model/User.php
+++ adApi/model/User.php
@@ -9,17 +9,25 @@
         private $passwordHash;
 
         static function login(string $login, string $password, mysqli $db) : int{
-            $sql = "SELECT id FROM user WHERE email = ? AND passwordHash = ?";
+            $sql = "SELECT id, passwordHash FROM user WHERE email = ?";
             $query = $db->prepare($sql);
-            $query->bind_param('ss', $login, $password);
+            $query->bind_param('s', $login);
             $query->execute();
             $result = $query->get_result();
             if ($result->num_rows == 0) {
                 throw new Exeption('Invalid login or poassword');
             }else{
                 $user = $result->fetch_assoc();
-                return $user['id'];
-            }
+                $id = $user['id'];
+                $hash = $user['passwordHash'];
+
+                if(password_verify($password, $hash)){
+                    return $id;
+                } else{
+                    throw new Exeption('Invalid login or poassword');
+                }
+
+            } 
         }
     }
 ?>
