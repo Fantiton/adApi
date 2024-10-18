cfe2b738ba4ded834e48f7981f262546839be6e6
Filip <filipcelejewski@wp.pl>
2024-10-18 08:16:59 +0200

    add Class User + Token

---
+++ adApi/model/Token.php
@@ -0,0 +1,12 @@
+<?php 
+    namespace AdApi;
+
+    use mysqli;
+
+    Class User {
+        private $id;
+        private $token;
+        private $ip;
+        private $used_id;
+    }
+?>
---
+++ adApi/model/User.php
@@ -0,0 +1,11 @@
+<?php 
+    namespace AdApi;
+
+    use mysqli;
+
+    Class User {
+        private $id;
+        private $email;
+        private $passwordHash;
+    }
+?>
