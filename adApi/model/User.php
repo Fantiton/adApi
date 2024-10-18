70e762d29126fb1ce3c16d6fece74af0011a801e
Filip <filipcelejewski@wp.pl>
2024-10-18 08:50:19 +0200

    rizz user

--- adApi/model/User.php
+++ adApi/model/User.php
@@ -7,5 +7,19 @@
         private $id;
         private $email;
         private $passwordHash;
+
+        static function login(string $login, string $password, mysqli $db) : int{
+            $sql = "SELECT id FROM user WHERE email = ? AND passwordHash = ?";
+            $query = $db->prepare($sql);
+            $query->bind_param('ss', $login, $password);
+            $query->execute();
+            $result = $query->get_result();
+            if ($result->num_rows == 0) {
+                throw new Exeption('Invalid login or poassword');
+            }else{
+                $user = $result->fetch_assoc();
+                return $user['id'];
+            }
+        }
     }
 ?>
