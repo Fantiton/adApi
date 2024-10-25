805d26b2040fbf11937ea0f9ea760ba19793cf79
Fantiton <filipcelejewski@wp.pl>
2024-10-25 09:49:42 +0200

    getUserId()

--- adApi/model/Token.php
+++ adApi/model/Token.php
@@ -26,5 +26,19 @@
                 return true;
             }
         }
+
+        static function getUserId(string $token, mysqli $db) : int{
+            $sql = "SELECT user_id FROM token WHERE token = ? ORDER BY id DESC LIMIT 1";
+            $query = $db->prepare($sql);
+            $query->bind_param('s', $token);
+            $query->execute();
+            $result = $query->get_result();
+            if ($result->num_rows == 0) {
+                throw new Exception('Invalid token');
+            } else{
+                $row = $result->fetch_assoc();
+                return $row['user_id'];
+            }
+        }
     }
 ?>
