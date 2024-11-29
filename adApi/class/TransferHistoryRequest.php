2b268abfb3b83bfe451cb3bcd74f23e511cd5336
Fantiton <filipcelejewski@wp.pl>
2024-11-29 09:29:04 +0100

    Odokumentowanie klasy TransferHistoryRequest

--- adApi/class/TransferHistoryRequest.php
+++ adApi/class/TransferHistoryRequest.php
@@ -2,9 +2,13 @@
 namespace AdApi;
 use mysqli;
 
+/**
+ * Class TransferHistoryRequest
+ * klasa do obsługi żądania historii transferów
+ */
 class TransferHistoryRequest{
     private $token;
-
+    
     public function __construct(){
         $data = file_get_contents('php://input');
         $dataArray = json_decode($data, true);
